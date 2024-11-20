0e9558f3998a347eff3cc71189ccab4cc9949da5
VNDT-082 <133595162+VNDT-082@users.noreply.github.com>
2024-11-20 22:55:27 +0700

    design login form

--- src/components/core/FormControl.tsx
+++ src/components/core/FormControl.tsx
@@ -1,22 +1,41 @@
 import { useId, useState } from "react";
 
 interface FormControlItem {
-    label: string
+    label: string,
+    className: string,
+    labelType: number,
+    type:string
 }
 
 const FormControl = (props: FormControlItem) => {
-    const { label } = props;
+    const { label, className, labelType,type } = props;
+
+    ////
+    // labelType = 0: background white outline none text white 
+    // labelType = 1: background white outline cyan text blue 950
+    ////
+
     const id = useId();
 
     const [labelState, setLabelState] = useState<boolean>(false);
 
     return (
         <div className="relative w-full flex justify-center items-center mx-auto my-6">
-            <input id={`input_form_element_${id}`} className={`input_form_element font-bold
-             ${!labelState ? 'text-white' : 'text-blue-950'}`} type="text"
-                onFocus={() => setLabelState(true)} onBlur={() => { setLabelState(false) }} />
-            <label htmlFor={`input_form_element_${id}`} id={`input_form_label_${id}`}
-                className={`input_form_label w-full  ${labelState ? 'transform -translate-y-[22px] text-white' : 'mb-0 text-blue-950'}`}>{label}</label>
+            <input id={`input_form_element_${id}`} className={`${className} font-bold
+             ${!labelState ? 'text-white' : 'text-blue-950'}`} type={ `${type}`}
+                onFocus={() => setLabelState(true)} onBlur={(e) => {
+                    if(e.target.value=='')
+                        setLabelState(false)
+                }} />
+            {labelType == 0 ?
+                <label htmlFor={`input_form_element_${id}`} id={`input_form_label_${id}`}
+                    className={`input_form_label w-full  ${labelState ? 'transform -translate-y-[22px] text-white' : 'mb-0 text-blue-950'}`}>{label}</label>
+                :
+                <label htmlFor={`input_form_element_${id}`} id={`input_form_label_${id}`}
+                className={`input_form_label w-full  text-blue-950 ${labelState ? 'transform -translate-y-[22px]' : 'mb-0'}`}>{label}</label>
+
+
+            }
 
         </div>
     );
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { showNotification } from "../redux/notificationSlice";
 import { ToastContainer } from "react-toastify";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NotificationContext } from "../components/context/NotificationContext";
 import { transform } from "typescript";
 import FormControl from "../components/core/FormControl";
@@ -12,7 +12,7 @@ const LoginPage = () => {
     // const handleShowNotification = () => {
     //     dispatch(showNotification("This is a toast notification!"));
     // };
-
+    const [isFormLogin, setIsFormLogin] = useState<boolean>(true);
     const notifyContext = useContext(NotificationContext);
     if (!notifyContext) {
         throw new Error('SomeComponent must be used within a NotifyProvider');
@@ -21,29 +21,70 @@ const LoginPage = () => {
 
     return (
         <>
-            <div className="w-full h-full fixed bg-white flex justify-center items-center">
+            <div className="w-full h-full fixed bg-white flex justify-center items-center"
+                style={{ backgroundImage: "url('bg.jpg')" }}>
                 <div className="w-10/12 sm:w-8/12 md:w-8/12 lg:w-3/12 bg-blue-800 rounded-md min-w-[320px]
-                shadow-blue-950 shadow-md">
-                    <div className="w-full h-full flex justify-center px-[20px] py-[20px]">
-                        <form className="w-full h-full">
-                            <p className="text-lg font-bold text-white text-center my-2">Sign up</p>
+                shadow-blue-950 shadow-md max-h-[360px] h-[360px] overflow-hidden relative">
+                    <div className="w-full h-full flex justify-center px-[20px] py-[20px] absolute">
+                        {isFormLogin ? <p className="text-lg font-bold text-white text-center my-2 cursor-pointer
+                            hover:scale-[1.01]"
+                            onClick={() => setIsFormLogin(false)}>Sign up</p> : null}
+                        <form className={`w-full h-full ${!isFormLogin ? '' : 'hidden'}`}>
+                            <p className="text-lg font-bold text-white text-center my-2 cursor-pointer
+                            hover:scale-105"
+                                onClick={() => setIsFormLogin(false)}>Sign up</p>
                             <div className="my-4 flex justify-center items-center ">
-                                <FormControl key={'fullname'} label="Full name" />
+                                <FormControl key={'fullname'} label="Full name" className="input_form_element"
+                                    labelType={0} type="text" />
                             </div>
 
                             <div className="my-4 flex justify-center items-center ">
-                                <FormControl key={'username'} label="User name" />
+                                <FormControl key={'username'} label="User name" className="input_form_element"
+                                    labelType={0} type="text" />
                             </div>
 
                             <div className="my-4 flex justify-center items-center ">
-                                <FormControl key={'password'} label="Password" />
+                                <FormControl key={'password'} label="Password " className="input_form_element"
+                                    labelType={0} type="password" />
+                            </div>
+
+                            <div className="my-4 flex justify-center items-center px-4 ">
+                                <button className="w-full my-2 bg-blue-600 h-[30px]
+                                rounded-md hover:bg-blue-700 hover:shadow-sm shadow-blue-950
+                                hover:font-bold hover:text-white">Sign up</button>
                             </div>
 
                         </form>
                     </div>
-                    <div className="bg-white">
-                        <form>
-                            <p>Login</p>
+                    <div className={`w-full h-full flex justify-center px-[20px] py-[20px] bg-white
+                    rounded-t-md
+                      shadow-sm shadow-gray-400 absolute transition-transform duration-500 z-20 ease-in-out
+                      ${isFormLogin ? 'transform -translate-y-[180px] mt-[250px] ' : 'mt-[290px]'} `}>
+                        {!isFormLogin ? <p className="text-lg font-bold text-blue-950 text-center my-2 cursor-pointer
+                            hover:scale-[1.01] "  onClick={() => setIsFormLogin(true)}>Login</p> : null}
+
+                        <form className={`w-full h-full ${!isFormLogin ? 'hidden' : ''}`}>
+                            <p className="text-lg font-bold text-blue-950 text-center my-2 cursor-pointer
+                            hover:scale-105 " onClick={() => setIsFormLogin(true)}>Login</p>
+                            <div className="my-4 flex justify-center items-center ">
+                                <FormControl key={'username'} label="User name" className="input_form_element_v2"
+                                    labelType={1} type="text" />
+                            </div>
+
+                            <div className="my-4 flex justify-center items-center ">
+                                <FormControl key={'password'} label="Password " className="input_form_element_v2"
+                                    labelType={1} type="password" />
+                            </div>
+
+                            <div className="my-4 flex justify-center items-center px-4 ">
+                                <button className="w-full my-2 bg-blue-600 h-[30px]
+                                rounded-md hover:bg-blue-700 hover:shadow-sm shadow-blue-950
+                                hover:font-bold hover:text-white">Login</button>
+                            </div>
+                            <div className="my-4 flex justify-center items-center px-4 ">
+                                <button className="hover:scale-105">Forgot password</button>
+                            </div>
+
                         </form>
                     </div>
                 </div>
