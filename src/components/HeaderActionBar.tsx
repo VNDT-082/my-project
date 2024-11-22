e75784527869f5653456d119ea134877cc3d5125
VNDT-082 <133595162+VNDT-082@users.noreply.github.com>
2024-11-22 22:30:34 +0700

    add icon and slide bar

--- src/components/HeaderActionBar.tsx
+++ src/components/HeaderActionBar.tsx
@@ -1,8 +1,50 @@
+import IconConform from "./core/icons/IconConform";
+import IconDelete from "./core/icons/IconDelete";
+import IconEdit from "./core/icons/IconEdit";
+import IconPen from "./core/icons/IconPen";
+import IconSave from "./core/icons/IconSave";
+
 const HeaderActionBar = () => {
     return (
         <div className="w-full p-2 ">
-            <div className="bg-blue-800 w-[82%] h-[50px]  p-4 flex items-center  fixed  rounded-sm">
-                <p className="text-red-400 text-3xl">VONGUYENDUYTAN-082</p>
+            <div className="bg-blue-800 w-[82%] h-[50px]  p-4 flex items-center  fixed  rounded-md">
+                <ul className="flex flex-row">
+                    <li>
+                        <div className="flex flex-row text-white cursor-pointer">
+                            <IconSave />
+                            <p>Add</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="flex flex-row text-white mx-4 cursor-pointer">
+                            <IconPen />
+                            <p>Update</p>
+                        </div>
+                    </li>
+
+                    {/* <li>
+                        <div className="flex flex-row text-white mx-4">
+                            <IconEdit />
+                            <p>Update</p>
+                        </div>
+                    </li> */}
+
+                    <li>
+                        <div className="flex flex-row text-white mx-4 cursor-pointer">
+                            <IconDelete />
+                            <p>Delete</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="flex flex-row text-white mx-4 cursor-pointer ">
+                            <IconConform />
+                            <p>Conform</p>
+                        </div>
+                    </li>
+
+                </ul>
             </div>
         </div>
 
--- src/components/SlideBar.tsx
+++ src/components/SlideBar.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
 import AbpMenu from "../interface/AbpMenu";
+import IconHome from "./core/icons/IconHome";
+import IconSearch from "./core/icons/IconSearch";
+import IconBar from "./core/icons/IconBar";
+import IconBarFromLeft from "./core/icons/IconBarFromLeft";
 
 const SlideBar = () => {
 
@@ -10,16 +15,45 @@ const SlideBar = () => {
         { menu_name: 'Quản lý danh mục' },
         { menu_name: 'Danh mục vùng miền' },
         { menu_name: 'Danh mục phòng' }];
+
+    const [isActive, setIsActive] = useState<string>("");
+    const [showFullBar, setShowFullBar] = useState<boolean>(false)
+
     return (
         <div className="w-full h-full max-w-[320px] bg-blue-800">
             <div className="h-[60px]">VNDT</div>
             <ul>
+                <li>
+                    <div className="flex flex-row w-full px-3 py-2 items-center justify-end
+                    text-white font-bold"
+                        onClick={() => setShowFullBar(!showFullBar)}>
+                        <p className="w-11/12">Menu</p>
+                        {showFullBar ?
+                            <IconBar /> : <IconBarFromLeft />}
+                    </div>
+                </li>
+
+                <li>
+                    <div className="flex flex-row w-full px-3 py-2 items-center">
+                        <div className="absolute">
+                            <IconSearch />
+                        </div>
+
+                        <input className="w-full h-[25px] text-lg outline-[0.5px] pl-[25px]
+                         outline-cyan-400 rounded-sm focus:outline-none" type="text" placeholder="Search..." />
+                    </div>
+                </li>
                 {listMenu.map(item => {
-                    return <li className="text-lg text-white font-bold my-0
-                    px-3 py-2 w-full border-b border-t hover:scale-105 cursor-pointer">
-                        <p >
-                            {item.menu_name}
-                        </p>
+                    return <li className={`text-lg text-white font-bold w-full border-b border-t 
+                    cursor-pointer ${isActive == item.menu_name ? 'bg-white' : 'bg-transparent'}`}
+                        onClick={() => setIsActive(item.menu_name)}>
+                        <div className={`h-[4px] w-full rounded-br-full
+                            ${isActive == item.menu_name ? 'bg-blue-800' : 'bg-transparent'}`}></div>
+
+                        <p className={`hover:scale-105 flex flex-row  px-3 py-2 ${isActive == item.menu_name ? 'font-bold text-blue-950 bg-white' : 'text-white bg-transparent'}`}>
+                            <IconHome />{item.menu_name}</p>
+                        <div className={`h-[4px] w-full rounded-tr-full 
+                             ${isActive == item.menu_name ? 'bg-blue-800' : 'bg-transparent'}`}></div>
                     </li>
                 }
                 )}
